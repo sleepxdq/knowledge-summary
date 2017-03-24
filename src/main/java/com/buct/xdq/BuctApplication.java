8e0062a9284a0e352fa52933fceac3404fa6140d
sleepxdq <xu_dongqiang@foxmail.com>
2017-03-25 01:02:09 +0800

    set new project

---
+++ src/main/java/com/buct/xdq/BuctApplication.java
@@ -0,0 +1,12 @@
+package com.buct.xdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuctApplication.class, args);
+	}
+}
