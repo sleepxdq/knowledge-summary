5a63079bb6a2a266f59afc3079cfe1419ce8600c
Seven.Xu <xu_dongqiang@foxmail.com>
2021-10-27 23:35:01 +0800

    增加新题：重复数字排列组合

---
+++ exercise/src/main/java/com/xdq/t10/Solution.java
@@ -0,0 +1,31 @@
+package com.xdq.t10;
+
+import java.util.ArrayList;
+
+/**
+ * 有重复项数字的所有排列
+ * <p>
+ * 描述
+ * 给出一组可能包含重复项的数字，返回该组数字的所有排列。结果以字典序升序排列。
+ * <p>
+ * 数据范围： 0<n≤8 ，数组中的值满足 -1≤val≤5
+ * 要求：空间复杂度O(n!)，时间复杂度O(n!)
+ * <p>
+ * <p>
+ * 示例1
+ * 输入：[1,1,2]
+ * 返回值：[[1,1,2],[1,2,1],[2,1,1]]
+ * <p>
+ * 示例2
+ * 输入：[0,1]
+ * 返回值：[[0,1],[1,0]]
+ *
+ * @author xdq
+ */
+public class Solution {
+
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        return null;
+    }
+
+}
